cb13e473003afcbe427f7df947924a5f195489e0
Nicoalazar <nicozalazar67@gmail.com>
2024-03-15 09:22:34 -0300

    microdesafio1

---
+++ Microdesafios/app.js
@@ -0,0 +1,13 @@
+const changeText=()=>{
+document.getElementById('title').innerHTML= "Texto modificado con JS"
+}
+
+//Microdesafio S1
+
+let number = 3.14
+let string = 'Texto'
+let boolean = true
+
+console.log(number)
+console.log(string)
+console.log(boolean)
