53c2fd5a868e76c9ad7cbf165b47e25ed944186e
Nicoalazar <nicozalazar67@gmail.com>
2024-03-25 09:40:44 -0300

    carpeta complementarias

---
+++ Microdesafios/Complementarios/app.js
@@ -0,0 +1,33 @@
+const changeText=()=>{
+    document.getElementById('title').innerHTML= "Texto modificado con JS"
+    }
+//              Desafíos complementarios            //
+
+/*
+Actividad 01:
+Crea un algoritmo que le pida al usuario que ingrese un número y luego evalúe si ese
+número es par o impar.
+*/
+let numero = prompt('INGRESE UN NUMERO PARA SABER SI ES PAR O IMPAR');
+if (numero % 2 == 0) {
+    console.log('El numero ' + numero + ' es par');
+}else {
+    console.log('El numero ' + numero + ' es impar');
+}
+/*
+Actividad 02:
+Crea un algoritmo que le pida al usuario un número y luego utiliza while para evaluar
+cuántos números son pares y cuantos impares, hasta que el usuario ingrese "salir".*/
+let numero1 = prompt('INGRESE UN NUMERO PARA EVALUAR O INGRESE "SALIR" PARA SALIR');
+let par = 0;
+let impar = 0;
+while (numero1 != 'salir' && numero1 != 'SALIR') {
+    if (numero1 % 2 == 0) {
+        par++;
+    }else {
+        impar++;
+    }
+    numero1 = prompt('INGRESE UN NUMERO PARA EVALUAR O INGRESE "SALIR" PARA SALIR');
+}
+console.log('La cantidad de pares es ' + par);
+console.log('La cantidad de impares es ' + impar);
--- Microdesafios/app.js
+++ Microdesafios/app.js
@@ -49,39 +49,6 @@ Propuesta:
 let lados = prompt('INGRESE CANTIDAD DE LADOS');
 for (let index = 0; index < lados; index++) {
 if (index > 3) {
-    alert("Es un poligono");
-    break; //Detiene el bucle si tiene mas de 3 lados
-}else {
-    alert("No es un poligono");
 }
-
-//              Desafíos complementarios            //
-
-/*
-Actividad 01:
-Crea un algoritmo que le pida al usuario que ingrese un número y luego evalúe si ese
-número es par o impar.
-*/
-let numero = prompt('INGRESE UN NUMERO PARA SABER SI ES PAR O IMPAR');
-if (numero % 2 == 0) {
-    console.log('El numero ' + numero + ' es par');
-}else {
-    console.log('El numero ' + numero + ' es impar');
-}}
-/*
-Actividad 02:
-Crea un algoritmo que le pida al usuario un número y luego utiliza while para evaluar
-cuántos números son pares y cuantos impares, hasta que el usuario ingrese "salir".*/
-let numero1 = prompt('INGRESE UN NUMERO PARA EVALUAR O INGRESE "SALIR" PARA SALIR');
-let par = 0;
-let impar = 0;
-while (numero1 != 'salir' && numero1 != 'SALIR') {
-    if (numero1 % 2 == 0) {
-        par++;
-    }else {
-        impar++;
-    }
-    numero1 = prompt('INGRESE UN NUMERO PARA EVALUAR O INGRESE "SALIR" PARA SALIR');
+alert(`lado ${index+1}`);
 }
-console.log('La cantidad de pares es ' + par);
-console.log('La cantidad de impares es ' + impar);
